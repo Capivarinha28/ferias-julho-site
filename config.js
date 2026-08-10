fd42cdb5a7b82eb8962c663c330e69f0349fd259
Capivarinha28 <maria.andrade32@aluno.cps.sp.gov.br>
2026-08-10 02:07:26 -0300

    aa

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Preencha com os dados do SEU projeto Supabase
 // (Supabase -> Project Settings -> API)
-const SUPABASE_URL = "https://ldnbxrxopslbziyphvsg.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://ldnbxrxopslbziyphvsg.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_Vods8Z5knNaLs-yq0SJDTg_x9MMLsAD";
